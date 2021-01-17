f08e2afb15613fd54fdea3b1afef71da6130eec1
利民 石 <shi888888@hotmail.com>
2021-01-17 20:57:44 +0800

    shiro+jwt+redis

---
+++ shiro-springboot/src/main/java/com/example/demo/mapper/SysUserMapper.java
@@ -0,0 +1,19 @@
+package com.example.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.entity.SysUser;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * 用户表 Mapper 接口
+ */
+public interface SysUserMapper extends BaseMapper<SysUser> {
+    /**
+     * 通过用户账号查询用户信息
+     *
+     * @param username
+     * @return
+     */
+    public SysUser getUserByName(@Param("username") String username);
+
+}
